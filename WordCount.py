8b71f0bbe06d59a100dc2591a058d3963432d6c9
EdipUman <euman@unomaha.edu>
2026-02-28 16:07:50 -0600

    Update WordCount.py

--- WordCount.py
+++ WordCount.py
@@ -1,14 +1,26 @@
 #WordCount.py
-#Name:
-#Date:
-#Assignment:
+#Name:Edip Uman
+#Date: 02/28/2026
+#Assignment: Lab 6
+
 
 def main():
   textFile = open("gettysberg.txt", 'r')
+  lineCount = 0 
+  wordCount = 0
+  letterCount = 0 
   
   for line in textFile:
-    print(line)
-  
+    lineCount = lineCount + 1
+    letterCount = letterCount + len(line)
+    words = line.split()
+    for w  in words:
+      wordCount = wordCount + 1
+    
+
+  print("Words: ", wordCount)
+  print("Lines: ", lineCount)
+  print("Letters:", letterCount)
 
 if __name__ == '__main__':
   main()
