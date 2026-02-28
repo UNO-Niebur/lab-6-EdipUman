aaf69534870bc375678158555c967cc0e92c04d2
EdipUman <euman@unomaha.edu>
2026-02-28 16:57:13 -0600

    Update WordIndex.py
    lab 6 completed

--- WordIndex.py
+++ WordIndex.py
@@ -7,14 +7,25 @@ def main():
   textFile = open("fish.txt", 'r')
   
   words = {} #create an empty dictionary
+  lineNum = 0
   
-  
-  print ("fish" in words) #is a word already in the dictionary?
-  words["fish"] = [2]     #add a list to the dictionary
-  print ("fish" in words) #is the word there now?
-  words["fish"].append(5) #add to an existing list
-  print(words)
-
+  for line in textFile:
+    lineNum = lineNum + 1 
+    wordList = line.split()
+    for w in wordList:
+      w = w.lower()
+      w = w.replace("," , "")
+      w = w.replace("." , "")
+      w = w.replace("!" , "")
+    
+      if w in words:
+        if lineNum not in words[w]:
+          words[w].append(lineNum)
+      else: 
+        words[w] = [lineNum]
+        
+  for word in words:
+    print(word, words[word])
 
 if __name__ == '__main__':
   main()
